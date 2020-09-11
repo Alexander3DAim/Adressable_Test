490b4083975920f43d90d5bc67b8feba0de45c7c
Infandum <Infandum_alex@hotmail.be>
2020-09-11 11:40:06 +0200

    Set up

---
+++ 3DAT_Adressables_Demo/Assets/SceneSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public int index = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.P))
+        {
+            SceneManager.LoadScene(index);
+        }
+    }
+}
